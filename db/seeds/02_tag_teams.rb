3f7e79c4df6239de64de561d1aff38644db79586
sjschmidt93 <sjschmidt93@gmail.com>
2020-08-06 11:12:26 -0400

    more cleaning up of tag team seeds

--- db/seeds/02_tag_teams.rb
+++ db/seeds/02_tag_teams.rb
@@ -1,25 +1,20 @@
 require_relative '../seed_helpers'
 
-tag_teams = TagTeam.create([
-  { name: "Lucha Brothers", is_official: true },
-  { name: "The Butcher & the Blade", is_official: true },
-  { name: "Best Friends", is_official: true },
-  { name: "Santana & Ortiz", is_official: true }
-])
-
 SeedHelpers::create_tag_team("Le Sex Gods", true, ["Chris Jericho", "Sammy Guevara"])
 SeedHelpers::create_tag_team("Dark Order (Evil Uno & Stu Grayson)", true, ["Evil Uno", "Stu Grayson"])
-
-SeedHelpers::create_tag_team("SCU (Frankie Kazarian & Scorpio Sky)", true, ["Frankie Kazarian", "Scorpio Sky"])
-
-SeedHelpers::create_tag_team("Jurassic Express & Best Friends & Orange Cassidy", false, ["Chuck Taylor", "Trent", "Jungle Boy", "Luchasauras", "Orange Cassidy"])
-SeedHelpers::create_tag_team("Inner Circle", false, ["Chris Jericho", "Sammy Guevara", "Jake Hager", "Ortiz", "Santana"])
-
 SeedHelpers::create_tag_team("Kenny Omega & Adam Page", true, ["Kenny Omega", "Adam Page"])
 SeedHelpers::create_tag_team("The Young Bucks", true, ["Matt Jackson", "Nick Jackson"])
 SeedHelpers::create_tag_team("Private Party", true, ["Isiah Kassidy", "Marq Quen"])
+SeedHelpers::create_tag_team("SCU (Frankie Kazarian & Scorpio Sky)", true, ["Frankie Kazarian", "Scorpio Sky"])
+SeedHelpers::create_tag_team("Lucha Brothers", true, ["Pentagon Jr.", "Rey Fenix"])
+SeedHelpers::create_tag_team("The Butcher & the Blade", true, ["The Butcher", "The Blade"])
+SeedHelpers::create_tag_team("Best Friends", true, ["Chuck Taylor", "Trent"])
+SeedHelpers::create_tag_team("Santana & Ortiz", true, ["Santana", "Ortiz"])
 
 
 # unofficial
+SeedHelpers::create_tag_team("Jurassic Express & Best Friends & Orange Cassidy", false, ["Chuck Taylor", "Trent", "Jungle Boy", "Luchasauras", "Orange Cassidy"])
+SeedHelpers::create_tag_team("Inner Circle", false, ["Chris Jericho", "Sammy Guevara", "Jake Hager", "Ortiz", "Santana"])
+
 SeedHelpers::create_tag_team("Darby Allin & Jon Moxley", false, ["Darby Allin", "Jon Moxley"])
 SeedHelpers::create_tag_team("Brian Cage & Ricky Starks", false, ["Brian Cage", "Ricky Starks"])
