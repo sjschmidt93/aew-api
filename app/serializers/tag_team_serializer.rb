ab925f2eaf4d717ba41fdf3f06a3fc1fb7a41235
sjschmidt93 <sjschmidt93@gmail.com>
2020-10-04 15:25:39 -0500

    serialize sub teams

--- app/serializers/tag_team_serializer.rb
+++ app/serializers/tag_team_serializer.rb
@@ -1,7 +1,11 @@
 class TagTeamSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image_url, :wrestlers, :is_official
+  attributes :id, :name, :image_url, :wrestlers, :is_official, :sub_teams
 
   def wrestlers
     ActiveModel::SerializableResource.new(object.wrestlers,  each_serializer: WrestlerSerializer)
+  end
+
+  def sub_teams
+    ActiveModel::SerializableResource.new(object.sub_teams,  each_serializer: TagTeamSerializer)
   end
 end
